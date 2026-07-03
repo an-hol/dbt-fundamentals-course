039a219fb820c8e1e9b3d99e1ece56731fd84f99
aho@exerp.com <aho@exerp.com>
2026-07-03 13:51:08 +0000

    feat/adding-payments

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -8,8 +8,8 @@ orders as (
 
     select * from {{ ref('stg_jaffle_shop_orders') }}
 
-),
-
+)
+,
 customer_orders as (
 
     select
@@ -25,6 +25,14 @@ customer_orders as (
 
 ),
 
+lifetime_amount as
+(
+    select  
+    customer_id, sum(amount) as lifetime_amount 
+    from {{ ref('fact_orders') }} 
+    group by customer_id
+)
+,
 
 final as (
 
@@ -34,11 +42,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        lifetime_amount
     from customers
 
     left join customer_orders using (customer_id)
+
+    left join lifetime_amount using (customer_id)
 
 )
 
